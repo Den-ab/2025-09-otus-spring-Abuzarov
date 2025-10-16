4b41f6b3b1f16137db7a3ca00a75972072e13bed
dinis <dinis.abuzarov@mail.ru>
2025-10-17 02:09:07 +0400

    hw01 Вывод в консоль

--- hw01-xml-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw01-xml-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -1,16 +1,32 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
 
     private final IOService ioService;
 
+    private final QuestionDao questionDao;
+
     @Override
     public void executeTest() {
         ioService.printLine("");
         ioService.printFormattedLine("Please answer the questions below%n");
         // Получить вопросы из дао и вывести их с вариантами ответов
+        final List<Question> questions = questionDao.findAll();
+        questions.forEach((Question question) -> {
+
+            ioService.printLine(question.text());
+            ioService.printLine("");
+            question.answers().stream().map(Answer::text).forEach(ioService::printLine);
+            ioService.printLine("");
+        });
+
     }
 }
