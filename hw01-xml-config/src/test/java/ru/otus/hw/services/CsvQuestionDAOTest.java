0bb630b2de82bef2f501079a730714f3b309be7d
dinis <dinis.abuzarov@mail.ru>
2025-10-23 03:13:12 +0400

    hw01 Тесты

---
+++ hw01-xml-config/src/test/java/ru/otus/hw/services/CsvQuestionDAOTest.java
@@ -0,0 +1,40 @@
+package ru.otus.hw.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.CsvQuestionDao;
+import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Question;
+import ru.otus.hw.service.IOService;
+import ru.otus.hw.service.StreamsIOService;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class CsvQuestionDAOTest {
+    private TestFileNameProvider fileNameProvider;
+
+    private IOService ioService;
+
+    private QuestionDao questionDao;
+
+    @BeforeEach
+    void setUp() {
+        fileNameProvider = mock(TestFileNameProvider.class);
+        ioService = new StreamsIOService(System.out);
+        questionDao = new CsvQuestionDao(fileNameProvider, ioService);
+    }
+
+    @DisplayName("Not empty question list")
+    @Test
+    void test1() {
+        when(this.fileNameProvider.getTestFileName()).thenReturn("questions.csv");
+        final List<Question> foundQuestions = this.questionDao.findAll();
+        assertThat(foundQuestions).isNotEmpty();
+    }
+}
