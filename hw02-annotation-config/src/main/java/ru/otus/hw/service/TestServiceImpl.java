bad998a3afd13d91e40e47c3c4287b754a1c3f3a
dinis <dinis.abuzarov@mail.ru>
2025-11-06 14:52:56 +0400

    hw02 Логика тестирования

--- hw02-annotation-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw02-annotation-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -3,8 +3,12 @@ package ru.otus.hw.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
 import ru.otus.hw.domain.Student;
 import ru.otus.hw.domain.TestResult;
+
+import java.util.List;
+import java.util.stream.IntStream;
 
 @RequiredArgsConstructor
 @Service
@@ -22,7 +26,19 @@ public class TestServiceImpl implements TestService {
         var testResult = new TestResult(student);
 
         for (var question: questions) {
-            var isAnswerValid = false; // Задать вопрос, получить ответ
+            ioService.printLine("\n" + question.text());
+            final List<Answer> answers = question.answers();
+            IntStream.range(0, answers.size()).forEach(index -> {
+                ioService.printLine(String.format("%s. %s", index + 1, answers.get(index).text()));
+            });
+            var studentAnswer = ioService.readStringWithPrompt("\nPlease input correct option number").trim();
+            var isAnswerValid = false;
+
+            if (studentAnswer.matches("[1-9]")) {
+                final int index = Integer.parseInt(studentAnswer) - 1;
+                isAnswerValid = index < answers.size() && answers.get(index).isCorrect();
+            }
+
             testResult.applyAnswer(question, isAnswerValid);
         }
         return testResult;
