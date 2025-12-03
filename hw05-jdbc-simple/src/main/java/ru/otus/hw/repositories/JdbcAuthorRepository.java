83111f9118050a6369db8f074907423a350d9509
dinis <dinis.abuzarov@mail.ru>
2025-12-04 03:45:46 +0400

    hw05 Операции с БД для Author

--- hw05-jdbc-simple/src/main/java/ru/otus/hw/repositories/JdbcAuthorRepository.java
+++ hw05-jdbc-simple/src/main/java/ru/otus/hw/repositories/JdbcAuthorRepository.java
@@ -1,33 +1,55 @@
 package ru.otus.hw.repositories;
 
+import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.stereotype.Repository;
 import ru.otus.hw.models.Author;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
 public class JdbcAuthorRepository implements AuthorRepository {
 
+    private final NamedParameterJdbcOperations namedParameterJdbcOperations;
+
+    private final JdbcOperations jdbcOperations;
+
+    public JdbcAuthorRepository(NamedParameterJdbcOperations namedParameterJdbcOperations) {
+        this.namedParameterJdbcOperations = namedParameterJdbcOperations;
+        this.jdbcOperations = namedParameterJdbcOperations.getJdbcOperations();
+    }
+
     @Override
     public List<Author> findAll() {
-        return new ArrayList<>();
+        return this.jdbcOperations.query("SELECT id, full_name FROM authors", new AuthorRowMapper());
     }
 
     @Override
     public Optional<Author> findById(long id) {
-        return Optional.empty();
+
+        final Map<String, Object> queryParams = new HashMap<>(1);
+        queryParams.put("id", id);
+        final Author author = this.namedParameterJdbcOperations.queryForObject(
+            "SELECT id, full_name FROM authors WHERE id = :id",
+            queryParams,
+            new AuthorRowMapper()
+        );
+        return Optional.of(author);
     }
 
     private static class AuthorRowMapper implements RowMapper<Author> {
 
         @Override
         public Author mapRow(ResultSet rs, int i) throws SQLException {
-            return null;
+            final long id = rs.getLong("id");
+            final String fullName = rs.getString("full_name");
+            return new Author(id, fullName);
         }
     }
 }
