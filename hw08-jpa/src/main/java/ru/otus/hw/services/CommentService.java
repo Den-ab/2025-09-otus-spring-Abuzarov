2009421e49e9e5ffb854475a1f45aeb86cfc114c
dinis <dinis.abuzarov@mail.ru>
2026-01-20 18:18:26 +0400

    hw08 Копирование кода предыдущего дз

---
+++ hw08-jpa/src/main/java/ru/otus/hw/services/CommentService.java
@@ -0,0 +1,19 @@
+package ru.otus.hw.services;
+
+import ru.otus.hw.dto.CommentDTO;
+import ru.otus.hw.models.Comment;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CommentService {
+    Optional<CommentDTO> findById(long id);
+
+    List<CommentDTO> findByBookId(long id);
+
+    CommentDTO insert(String content, long bookId);
+
+    CommentDTO update(long id, String content, long bookId);
+
+    void deleteById(long id);
+}
