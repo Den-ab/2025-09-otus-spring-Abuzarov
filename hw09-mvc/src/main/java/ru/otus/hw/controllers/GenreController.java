25043255e224f5aaff9de04ee6f00a369c9ef3fd
dinis <dinis.abuzarov@mail.ru>
2026-02-04 01:07:02 +0400

    hw09 Страница жанров

--- hw09-mvc/src/main/java/ru/otus/hw/controllers/GenreController.java
+++ hw09-mvc/src/main/java/ru/otus/hw/controllers/GenreController.java
@@ -1,21 +1,26 @@
 package ru.otus.hw.controllers;
 
 import lombok.RequiredArgsConstructor;
-import ru.otus.hw.converters.GenreConverter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import ru.otus.hw.dto.GenreDTO;
 import ru.otus.hw.services.GenreService;
 
-import java.util.stream.Collectors;
+import java.util.List;
 
 @RequiredArgsConstructor
+@Controller
 public class GenreController {
 
     private final GenreService genreService;
 
-    private final GenreConverter genreConverter;
+    @RequestMapping(value = "/genres", method = RequestMethod.GET)
+    public String findAllGenres(Model model) {
 
-    public String findAllGenres() {
-        return genreService.findAll().stream()
-                .map(genreConverter::genreToString)
-                .collect(Collectors.joining("," + System.lineSeparator()));
+        final List<GenreDTO> genres = genreService.findAll();
+        model.addAttribute("genres", genres);
+        return "genres";
     }
 }
