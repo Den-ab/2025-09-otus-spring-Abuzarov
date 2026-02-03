552b2fb01bd2bf9aba991ace98b79f46cdd2cbda
dinis <dinis.abuzarov@mail.ru>
2026-02-04 01:02:29 +0400

    hw09 Страница авторов

--- hw09-mvc/src/main/java/ru/otus/hw/controllers/AuthorController.java
+++ hw09-mvc/src/main/java/ru/otus/hw/controllers/AuthorController.java
@@ -5,11 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import ru.otus.hw.converters.AuthorConverter;
+import ru.otus.hw.dto.AuthorDTO;
 import ru.otus.hw.services.AuthorService;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Controller
@@ -17,15 +16,11 @@ public class AuthorController {
 
     private final AuthorService authorService;
 
-    private final AuthorConverter authorConverter;
-
     @RequestMapping(value = "/authors", method = RequestMethod.GET)
     public String findAllAuthors(Model model) {
 
-        final List<String> authorsString = authorService.findAll().stream()
-            .map(authorConverter::authorToString)
-            .collect(Collectors.toList());
-        model.addAttribute("authors", authorsString);
+        final List<AuthorDTO> authors = authorService.findAll();
+        model.addAttribute("authors", authors);
         return "authors";
     }
 }
