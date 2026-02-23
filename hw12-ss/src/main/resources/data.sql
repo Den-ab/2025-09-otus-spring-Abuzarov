86d0f0d4e0c0883560d8c0220f0ecb8c40dd21e6
dinis <dinis.abuzarov@mail.ru>
2026-02-23 23:25:16 +0400

    hw12 Создание записей в БД

--- hw12-ss/src/main/resources/data.sql
+++ hw12-ss/src/main/resources/data.sql
@@ -9,3 +9,7 @@ values ('BookTitle_1', 1, 1), ('BookTitle_2', 2, 2), ('BookTitle_3', 3, 3);
 
 insert into comments(content, book_id)
 values ('Comment_1', 1), ('Comment_2', 2), ('Comment_3', 3);
+
+-- Хеш указан для пароля 123 (через BCrypt)
+insert into users(login, password)
+values ('test.login', '$2a$12$vx3lV6uUn/RN.32k/y0UyOeOGmRoRgsnh3o9CWS8M5ik1fjoTckoW');
