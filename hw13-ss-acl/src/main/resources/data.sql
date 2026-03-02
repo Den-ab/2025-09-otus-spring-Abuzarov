3289954cb2ddf914a657f52e3289922a573af77d
dinis <dinis.abuzarov@mail.ru>
2026-03-02 23:07:06 +0400

    hw13 Схемы для ACL и заполнение БД

--- hw13-ss-acl/src/main/resources/data.sql
+++ hw13-ss-acl/src/main/resources/data.sql
@@ -11,5 +11,85 @@ insert into comments(content, book_id)
 values ('Comment_1', 1), ('Comment_2', 2), ('Comment_3', 3);
 
 -- Хеш указан для пароля 123 (через BCrypt)
-insert into users(login, password)
-values ('test.login', '$2a$12$vx3lV6uUn/RN.32k/y0UyOeOGmRoRgsnh3o9CWS8M5ik1fjoTckoW');
+insert into users(id, login, password, role) values
+    (1, 'test.observer', '$2a$12$vx3lV6uUn/RN.32k/y0UyOeOGmRoRgsnh3o9CWS8M5ik1fjoTckoW', 'ROLE_USER_OBSERVER'),
+    (2, 'test.editor', '$2a$12$vx3lV6uUn/RN.32k/y0UyOeOGmRoRgsnh3o9CWS8M5ik1fjoTckoW', 'ROLE_USER_EDITOR'),
+    (3, 'test.admin', '$2a$12$vx3lV6uUn/RN.32k/y0UyOeOGmRoRgsnh3o9CWS8M5ik1fjoTckoW', 'ROLE_SUPER_ADMIN');
+
+insert into acl_sid (id, principal, sid) values
+    (1, 0, 'ROLE_USER_OBSERVER'),
+    (2, 0, 'ROLE_USER_EDITOR'),
+    (3, 0, 'ROLE_SUPER_ADMIN');
+
+insert into acl_class (id, class) values
+    (1, 'ru.otus.hw.models.Book'),
+    (2, 'ru.otus.hw.models.Genre'),
+    (3, 'ru.otus.hw.models.Author'),
+    (4, 'ru.otus.hw.models.Comment');
+
+insert into acl_object_identity (id, object_id_class, object_id_identity, parent_object, owner_sid, entries_inheriting) values
+    (1, 1, 1, NULL, 3, 0),
+    (2, 1, 2, NULL, 3, 0),
+    (3, 1, 3, NULL, 3, 0),
+    (4, 2, 1, NULL, 3, 0),
+    (5, 2, 2, NULL, 3, 0),
+    (6, 2, 3, NULL, 3, 0),
+    (7, 3, 1, NULL, 3, 0),
+    (8, 3, 2, NULL, 3, 0),
+    (9, 3, 3, NULL, 3, 0),
+    (10, 4, 1, NULL, 3, 0),
+    (11, 4, 2, NULL, 3, 0),
+    (12, 4, 3, NULL, 3, 0);
+
+-- маска 1 - только чтение
+-- маска 11 - это CRUD (чтение + ред + удаление): 1 + 2 + 8
+-- маска 16 - это админские: 16
+INSERT INTO acl_entry (acl_object_identity, ace_order, sid, mask, granting, audit_success, audit_failure) VALUES
+    -- Book ID = 1
+    (1, 0, 1, 1, 1, 1, 1),
+    (1, 1, 2, 11, 1, 1, 1),
+    (1, 2, 3, 16, 1, 1, 1),
+    -- Book ID = 2
+    (2, 0, 1, 1, 1, 1, 1),
+    (2, 1, 2, 11, 1, 1, 1),
+    (2, 2, 3, 16, 1, 1, 1),
+    -- Book ID = 3
+    (3, 0, 1, 1, 1, 1, 1),
+    (3, 1, 2, 11, 1, 1, 1),
+    (3, 2, 3, 16, 1, 1, 1),
+    -- Genre ID = 1
+    (4, 0, 1, 1, 1, 1, 1),
+    (4, 1, 2, 11, 1, 1, 1),
+    (4, 2, 3, 16, 1, 1, 1),
+    -- Genre ID = 2
+    (5, 0, 1, 1, 1, 1, 1),
+    (5, 1, 2, 11, 1, 1, 1),
+    (5, 2, 3, 16, 1, 1, 1),
+    -- Genre ID = 3
+    (6, 0, 1, 1, 1, 1, 1),
+    (6, 1, 2, 11, 1, 1, 1),
+    (6, 2, 3, 16, 1, 1, 1),
+    -- Author ID = 1
+    (7, 0, 1, 1, 1, 1, 1),
+    (7, 1, 2, 11, 1, 1, 1),
+    (7, 2, 3, 16, 1, 1, 1),
+    -- Author ID = 2
+    (8, 0, 1, 1, 1, 1, 1),
+    (8, 1, 2, 11, 1, 1, 1),
+    (8, 2, 3, 16, 1, 1, 1),
+    -- Author ID = 3
+    (9, 0, 1, 1, 1, 1, 1),
+    (9, 1, 2, 11, 1, 1, 1),
+    (9, 2, 3, 16, 1, 1, 1),
+    -- Comment ID = 1
+    (10, 0, 1, 1, 1, 1, 1),
+    (10, 1, 2, 11, 1, 1, 1),
+    (10, 2, 3, 16, 1, 1, 1),
+    -- Comment ID = 2
+    (11, 0, 1, 1, 1, 1, 1),
+    (11, 1, 2, 11, 1, 1, 1),
+    (11, 2, 3, 16, 1, 1, 1),
+    -- Comment ID = 3
+    (12, 0, 1, 1, 1, 1, 1),
+    (12, 1, 2, 11, 1, 1, 1),
+    (12, 2, 3, 16, 1, 1, 1);
--- hw13-ss-acl/src/main/resources/schema.sql
+++ hw13-ss-acl/src/main/resources/schema.sql
@@ -29,5 +29,68 @@ create table if not exists users (
     id bigserial,
     login varchar(255),
     password text,
+    role varchar(255),
     primary key (id)
 );
+
+create table if not exists acl_sid (
+    id bigint not null AUTO_INCREMENT,
+    principal tinyint not null,
+    sid varchar(100) not null,
+    primary key (id),
+    constraint unique_uk_1 unique (sid,principal)
+);
+
+create table if not exists acl_class (
+    id bigint not null AUTO_INCREMENT,
+    class varchar(255) not null,
+    primary key (id),
+    constraint unique_uk_2 unique (class)
+);
+
+create table if not exists acl_entry (
+    id bigint not null AUTO_INCREMENT,
+    acl_object_identity bigint not null,
+    ace_order int not null,
+    sid bigint not null,
+    mask int not null,
+    granting tinyint not null,
+    audit_success tinyint not null,
+    audit_failure tinyint not null,
+    primary key (id),
+    constraint unique_uk_4 unique (acl_object_identity,ace_order)
+);
+
+create table if not exists acl_object_identity (
+    id bigint not null AUTO_INCREMENT,
+    object_id_class bigint not null,
+    object_id_identity bigint not null,
+    parent_object bigint default null,
+    owner_sid bigint default null,
+    entries_inheriting tinyint not null,
+    primary key (id),
+    constraint unique_uk_3 unique (object_id_class,object_id_identity)
+);
+
+alter table acl_entry
+    add foreign key (acl_object_identity) references acl_object_identity(id);
+
+alter table acl_entry
+    add foreign key (sid) references acl_sid(id);
+
+--
+-- Constraints for table acl_object_identity
+--
+alter table acl_object_identity
+    add foreign key (parent_object) references acl_object_identity (id);
+
+alter table acl_object_identity
+    add foreign key (object_id_class) references acl_class (id);
+
+alter table acl_object_identity
+    add foreign key (owner_sid) references acl_sid (id);
+
+alter table acl_sid alter column id restart with 10;
+alter table acl_class alter column id restart with 10;
+alter table acl_object_identity alter column id restart with 20;
+alter table acl_entry alter column id restart with 100;
