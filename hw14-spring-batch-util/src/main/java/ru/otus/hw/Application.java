f57439045d0d1657ce5a743049803ccf2c57827b
dinis <dinis.abuzarov@mail.ru>
2026-03-16 00:58:37 +0400

    hw14 Лишние импорты

--- hw14-spring-batch-util/src/main/java/ru/otus/hw/Application.java
+++ hw14-spring-batch-util/src/main/java/ru/otus/hw/Application.java
@@ -2,13 +2,8 @@ package ru.otus.hw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ApplicationContext;
-import ru.otus.hw.config.AppProperties;
-import ru.otus.hw.service.TestRunnerService;
 
 @SpringBootApplication
-@EnableConfigurationProperties(AppProperties.class)
 public class Application {
     public static void main(String[] args) {
 
